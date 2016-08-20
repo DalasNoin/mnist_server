519f05197899502010237e7cfe18be6efd88a915
SimonDaalas <sim.ler68@gmail.com>
2016-08-20 22:23:27 +0200

    .

--- setup.sh
+++ setup.sh
@@ -7,8 +7,11 @@ export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorf
 sudo pip install --upgrade $TF_BINARY_URL
 
 echo Y | sudo apt-get install python-numpy python-scipy pyyaml h5py
+
+sudo pip install keras
 cd ~/.keras
 sudo python changename.py
+cd ~/mnist_server
 
 sudo pip install cython
 echo Y | sudo apt-get install libhdf5-dev
@@ -16,6 +19,6 @@ sudo pip install h5py
 
 sudo pip install Pillow
 
-cd ./Server
+cd ~/mnist_server/Server
 nohup sudo python SimpleHTTPServerWithUpload.py 80 &
 disown
