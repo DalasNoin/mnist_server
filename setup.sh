93d6f62dd9e146dcff7c63703fb8e683a3bfc88c
SimonDaalas <sim.ler68@gmail.com>
2016-08-20 23:19:21 +0200

    .

--- setup.sh
+++ setup.sh
@@ -11,14 +11,13 @@ sudo pip install --upgrade $TF_BINARY_URL
 echo Y | sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 echo Y | sudo apt-get install pyyaml h5py
 
+echo Y | sudo apt-get install libhdf5-dev
+sudo pip install h5py
+
 sudo pip install keras
 
 sudo rm ~/.keras/keras.json
-sudo mv keras.json ~/.keras/keras.json
-
-
-echo Y | sudo apt-get install libhdf5-dev
-sudo pip install h5py
+sudo cp keras.json ~/.keras/keras.json
 
 sudo pip install Pillow
 
